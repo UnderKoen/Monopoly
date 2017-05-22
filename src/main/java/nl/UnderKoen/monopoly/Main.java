337f3564c55c8d2907fce300b02cbf27cd414658
Koen van Staveren <koenvanstaveren@hotmail.com>
2017-05-22 13:27:43 +0200

    start

--- src/main/java/nl/UnderKoen/monopoly/Main.java
+++ src/main/java/nl/UnderKoen/monopoly/Main.java
@@ -1,7 +1,19 @@
 package nl.UnderKoen.monopoly;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * Created by Under_Koen on 22-05-17.
  */
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
---
+++ src/main/java/nl/UnderKoen/monopoly/entities/Player.java
@@ -0,0 +1,10 @@
+package nl.UnderKoen.monopoly.entities;
+
+/**
+ * Created by Under_Koen on 22-05-17.
+ */
+public interface Player {
+    String getName();
+
+    Inventory getInventory();
+}
