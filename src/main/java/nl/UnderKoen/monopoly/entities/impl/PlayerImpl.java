5c55780914ee99496c6ba7e14c6ee9e800de19b4
Koen van Staveren <koenvanstaveren@hotmail.com>
2017-05-22 20:54:28 +0200

    player impl

---
+++ src/main/java/nl/UnderKoen/monopoly/entities/impl/PlayerImpl.java
@@ -0,0 +1,30 @@
+package nl.UnderKoen.monopoly.entities.impl;
+
+import nl.UnderKoen.monopoly.entities.Inventory;
+import nl.UnderKoen.monopoly.entities.Player;
+
+/**
+ * Created by Under_Koen on 22-05-17.
+ */
+public class PlayerImpl implements Player {
+    private String name;
+    private Inventory inventory;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
+}
